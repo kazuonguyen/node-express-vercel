ddd5416d456a6ac6ffdb620aac1fb682cd49cb35
Kazuo Nguyen <61673166+kazuonguyen@users.noreply.github.com>
2023-07-20 17:19:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,14 @@
-const express = require('express')
-const app = express();
-const mongodb = require("mongodb")
-const cors = require('cors');   
-var MongoClient = require('mongodb').MongoClient;
-const  ObjectID = require('mongodb').ObjectID;
+// Import packages
+const express = require("express");
 const home = require("./routes/home");
 
-const port = process.env.PORT || 3000;
-var url = "mongodb+srv://kazuo:22445588@cluster0.7kibo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-app.use(express.json())
- app.use(cors())
- app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  });
-  app.use("/home", home);
+// Middlewares
+const app = express();
+app.use(express.json());
 
- app.listen(port,function () {
-  console.log('CORS-enabled web server listening on port',port);
-})
+// Routes
+app.use("/home", home);
+
+// connection
+const port = process.env.PORT || 9001;
+app.listen(port, () => console.log(`Listening to port ${port}`));
